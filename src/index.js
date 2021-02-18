cc36c4b9f086c006cd46920078b1298e04f55003
Alfredo <alfredomeschis@yahoo.com>
2021-02-18 14:11:09 +0100

    add search-bar in index.html and similar-movie in movie.html

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 
-const stripeMoviesPopular = document.querySelector("section.popular-movies")
-const stripeMoviesTopRated = document.querySelector("section.topRated-movies")
-const stripeUncoming = document.querySelector("section.uncoming-movies")
+const stripeMoviesPopular = document.querySelector("section.popular-movies");
+const stripeMoviesTopRated = document.querySelector("section.topRated-movies");
+const stripeUncoming = document.querySelector("section.uncoming-movies");
+const stripeSarch = document.querySelector("section.search-movies");
 
+const toastSearch = document.querySelector("div.search-toast");
+const btnSearch = document.querySelector("h2.search-toggle");
+const inputSearchMovie = document.querySelector("input#input-movie");
+const btnInputMovie = document.querySelector("button.btn-movie");
+const formMovie = document.querySelector("form.form-movie");
+
+let arrMovie = [];
 
 const state = {
     config: {
@@ -10,9 +18,8 @@ const state = {
         base_url: "https://api.themoviedb.org/3",
         base_url_poster: "https://image.tmdb.org/t/p/w500"
     },
-}
+};
 
-let arrMovie = []
 
 // get data to API
 function getUrl(path) {
@@ -23,9 +30,11 @@ function getUrl(path) {
     return `${base_url}${path}?api_key=${api_key}`;
 }
 
-const moviesPopular = getUrl("/movie/popular")
-const moviesTopRated = getUrl("/movie/top_rated")
-const uncoming = getUrl("/movie/upcoming")
+const moviesPopular = getUrl("/movie/popular");
+const moviesTopRated = getUrl("/movie/top_rated");
+const moviesUncoming = getUrl("/movie/upcoming");
+
+
 
 async function getData(url) {
 
@@ -35,7 +44,7 @@ async function getData(url) {
         if (!response.ok) {
             throw result;
         } 
-        console.log("questo è il result di get data ", result)
+        console.log(result);
         return result;
     } catch (errMess) {
         console.error(errMess);
@@ -49,50 +58,75 @@ function renderCard(movieSection) {
     arrMovie.forEach(movie => {
         const {base_url_poster} = state.config;
 
-        const link = document.createElement("a")
-        const card = document.createElement("div")
+        const link = document.createElement("a");
+        const card = document.createElement("div");
         const img = document.createElement("img");
-        const title = document.createElement("h4")
-        const stars = document.createElement("div")
+        const title = document.createElement("h4");
+        const stars = document.createElement("div");
 
-        link.classList.add("card-link")
-        card.classList.add("card-movie")
-        img.classList.add("card-movie-img")
-        title.classList.add("card-movie-title")
-        stars.classList.add("stars")
+        link.classList.add("card-link");
+        card.classList.add("card-movie");
+        img.classList.add("card-movie-img");
+        title.classList.add("card-movie-title");
+        stars.classList.add("stars");
 
-        movieSection.appendChild(link)
-        link.appendChild(card)
-        card.appendChild(img)
-        card.appendChild(stars)
-        card.appendChild(title)
+        movieSection.appendChild(link);
+        link.appendChild(card);
+        card.appendChild(img);
+        card.appendChild(stars);
+        card.appendChild(title);
 
 
         
-        link.href = `movie.html?id=${movie.id}`
-        title.textContent = movie.title?movie.title:movie.name
-        img.src = `${base_url_poster}${movie.poster_path}`
-        stars.style = `--rating: ${movie.vote_average/2}`
+        link.href = `movie.html?id=${movie.id}`;
+        title.textContent = movie.title?movie.title:movie.name;
+        img.src = `${base_url_poster}${movie.poster_path}`;
+        stars.style = `--rating: ${movie.vote_average/2}`;
     });
 
-}
+};
+
 // render card on stripe
 async function renderStripe(moviesFamily, moviesSection) {
 
     const result = await getData(moviesFamily).then ((result) => {
-        return result
+        return result;
     })
 
     arrMovie =  [...result.results];
-    console.log("questo è lo result: ",arrMovie);
-    renderCard(moviesSection)
+    renderCard(moviesSection);
 
 }
 
-console.log("questo è lo state: ",state);
+function renderSearchMovie() {
+
     
-renderStripe(moviesPopular, stripeMoviesPopular)
-renderStripe(moviesTopRated, stripeMoviesTopRated)
-renderStripe(uncoming, stripeUncoming)
+    const {base_url, api_key} = state.config;
+    const pippo = `${base_url}/search/movie?api_key=${api_key}&language=en-US&query=${inputSearchMovie.value}&page=1&include_adult=false`;
+    renderStripe(pippo,stripeSarch) ;
+    
+    while (stripeSarch.hasChildNodes()) {
+        stripeSarch.removeChild(stripeSarch.lastChild);
+    }
+}
+
+btnSearch.addEventListener("click", (evt) => {
+    toastSearch.classList.toggle("is-show");
+    evt.preventDefault;
+})
+
+inputSearchMovie.addEventListener("input", (evt) => 
+    setTimeout(() => {
+        
+        console.log(inputSearchMovie.value);
+        renderSearchMovie();
+        evt.preventDefault();
+        // toastSearch.classList.toggle("is-show");
+        
+}, 1000))
 
 
+
+renderStripe(moviesPopular, stripeMoviesPopular);
+renderStripe(moviesTopRated, stripeMoviesTopRated);
+renderStripe(moviesUncoming, stripeUncoming);
--- src/movie.js
+++ src/movie.js
@@ -1,31 +1,47 @@
-const container = document.querySelector("main.movie_conatiner")
-const img = document.querySelector("img.movie_img")
-const title = document.querySelector("h2.movie_title")
-const year = document.querySelector("h3.movie_year")
-const movieLength = document.querySelector("h3.movie_length")
-const description = document.querySelector("p.movie_description")
-const cast = document.querySelector("h3.movie_cast")
-const direction = document.querySelector("h3.movie_direction")
 
 const path = window.location.search;
 const id = path.split("=").pop();
 console.log(id);
 
+const container = document.querySelector("main.movie_conatiner");
+const img = document.querySelector("img.movie_img");
+const title = document.querySelector("h2.movie_title");
+const year = document.querySelector("h3.movie_year");
+const movieLength = document.querySelector("h3.movie_length");
+const description = document.querySelector("p.movie_description");
+const cast = document.querySelector("h3.movie_cast");
+const direction = document.querySelector("h3.movie_direction");
+const stripeSimilar = document.querySelector("section.stripe-movies");
+
 const state = {
     config: {
         base_url: "https://api.themoviedb.org/3/movie/",
         api_key: "4c92ecb713570027d15842178d4b610b",
-        base_url_poster: "https://image.tmdb.org/t/p/w500"
+        base_url_poster: "https://image.tmdb.org/t/p/w500",
+        language: "it-IT"
     }
 }
 
-// get data Details movie 
-async function getDetails() {
+
+const {api_key} = state.config;
+const {base_url} = state.config; 
+
+const urlSimilar = `${base_url}${id}/similar?api_key=${api_key}&language=${navigator.language}&page=1`;
+
+
+
+
+
+
+
+async function getData(url) {
+
     const {api_key} = state.config;
     const {base_url} = state.config; 
 
+
     try {
-        const response = await fetch(`${base_url}${id}?api_key=${api_key}&language=${state.config.language}`);
+        const response = await fetch(url);
         const result = await response.json();
     
         if (!response.ok) {
@@ -36,42 +52,74 @@ async function getDetails() {
         console.log(errMessage);
       }
 }
-async function getCredits() {
-    const {api_key} = state.config;
-    const {base_url} = state.config; 
 
-    try {
-        const response = await fetch(`${base_url}${id}/credits?api_key=${api_key}&language=${state.config.language}`);
-        const result = await response.json();
+
+// get data Details movie 
+// async function getDetails() {
+//     const {api_key} = state.config;
+//     const {base_url} = state.config; 
+
+//     try {
+//         const response = await fetch(`${base_url}${id}?api_key=${api_key}&language=${navigator.userLanguage}`);
+//         const result = await response.json();
     
-        if (!response.ok) {
-          throw result;
-        }
-        return result;
-    } catch (errMessage) {
-        console.log(errMessage);
-    }
+//         if (!response.ok) {
+//           throw result;
+//         }
+//         return result;
+//       } catch (errMessage) {
+//         console.log(errMessage);
+//       }
+// }
+// async function getCredits() {
+//     const {api_key} = state.config;
+//     const {base_url} = state.config; 
 
-}
+//     try {
+//         const response = await fetch(`${base_url}${id}/credits?api_key=${api_key}&language=${navigator.language}`);
+//         const result = await response.json();
+    
+//         if (!response.ok) {
+//           throw result;
+//         }
+//         return result;
+//     } catch (errMessage) {
+//         console.log(errMessage);
+//     }
+
+// }
 // render page with details and credit
 async function renderDetails() {
 
-    await getDetails().then ((result) => {
+    const {api_key} = state.config;
+    const {base_url} = state.config; 
+
+    
+
+    await getData(`${base_url}${id}?api_key=${api_key}&language=${navigator.userLanguage}`)
+        .then ((result) => {
 
         const {base_url_poster} = state.config; 
         
-        img.src = `${base_url_poster}${result.backdrop_path}`
-        title.textContent = `${result.title}`
+        img.src = `${base_url_poster}${result.backdrop_path}`;
+        title.textContent = `${result.title}`;
 
-        const date = new Date(result.release_date)
+        const date = new Date(result.release_date);
         year.textContent = `Anno: ${date.getFullYear()}`;
 
-        movieLength.textContent = `durata: ${result.runtime} minuti`
-        description.textContent = `${result.overview}`
+        movieLength.textContent = `durata: ${result.runtime} minuti`;
+        description.textContent = `${result.overview}`;
         
     })
 
-    await getCredits().then((result) => {
+}
+
+async function renderCredits() { 
+
+    
+ 
+    await getData(`${base_url}${id}/credits?api_key=${api_key}&language=${navigator.language}`)
+    .then((result) => {
 
         const actors = result.cast.slice(0, 5).map((actor) => actor.name);
         cast.textContent = `cast: ${actors}`;
@@ -86,5 +134,52 @@ async function renderDetails() {
 
 }
 
+function renderCard(movieSection) {
 
-renderDetails()
+    arrMovie.forEach(movie => {
+        const {base_url_poster} = state.config;
+
+        const link = document.createElement("a");
+        const card = document.createElement("div");
+        const img = document.createElement("img");
+        const title = document.createElement("h4");
+        const stars = document.createElement("div");
+
+        link.classList.add("card-link");
+        card.classList.add("card-movie");
+        img.classList.add("card-movie-img");
+        title.classList.add("card-movie-title");
+        stars.classList.add("stars");
+
+        movieSection.appendChild(link);
+        link.appendChild(card);
+        card.appendChild(img);
+        card.appendChild(stars);
+        card.appendChild(title);
+
+
+        
+        link.href = `movie.html?id=${movie.id}`;
+        title.textContent = movie.title?movie.title:movie.name;
+        img.src = `${base_url_poster}${movie.poster_path}`;
+        stars.style = `--rating: ${movie.vote_average/2}`;
+    });
+
+}
+
+async function renderStripe(moviesFamily, moviesSection) {
+
+    const result = await getData(moviesFamily).then ((result) => {
+        return result;
+    })
+
+    arrMovie =  [...result.results];
+    renderCard(moviesSection);
+
+}
+
+
+renderDetails();
+renderCredits();
+
+renderStripe(urlSimilar, stripeSimilar);
